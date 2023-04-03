744c9f3580517f18c2058918f8b2fe7f55e93e17
caseyclinch <clinchcasey@gmail.com>
2023-04-02 18:03:21 -0700

    completed project

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(dist){
+  if (dist <= 400)
+    return 'This one is on me!'
+  else if (dist <= 2000)
+    return 'That will be twenty bucks.'
+  else if (dist <= 2500)
+    return 'I will gladly take your thirty bucks.'
+  else return 'No can do.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let response = ''
+  city === 'NYC' ? (response = 'Ok, sounds good.') : (response =  'No go.')
+  return response
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(grat){
+  switch(grat) {
+    case 'generous':
+      return 'Thank you so much.'
+      break
+    case 'not as generous':
+      return 'Thank you.'
+      break
+    default :
+      return 'Bye.'
+    break
+  }
 }
